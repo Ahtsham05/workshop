3e110fb3eda1ab3587b65d5f5c6fe33174add735
Ahtsham05 <ahtshamyounas0321@gmail.com>
2025-08-27 01:21:46 +0500

    fix issues

--- client/src/routes/_authenticated/invoice.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/_authenticated/invoice')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/_authenticated/invoice"!</div>
-}
